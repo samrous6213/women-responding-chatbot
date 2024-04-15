26f17881b8fb997440fb94d3eda0b0e28364eeab
samrous6213 <138663805+samrous6213@users.noreply.github.com>
2024-04-15 22:27:17 +0100

    Update apps.py

--- django_chatbot/chatbot/apps.py
+++ django_chatbot/chatbot/apps.py
@@ -1,6 +1,9 @@
+# Importation de la classe AppConfig depuis le module django.apps
 from django.apps import AppConfig
 
-
+# Définition de la configuration de l'application Django "chatbot"
 class ChatbotConfig(AppConfig):
+    # Définition du champ automatique par défaut pour les modèles de base de données
     default_auto_field = "django.db.models.BigAutoField"
+    # Définition du nom de l'application
     name = "chatbot"
