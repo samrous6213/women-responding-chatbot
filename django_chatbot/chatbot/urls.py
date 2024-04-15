4bdfa28fbfc9e4cfdfb657f489495f68abdb6bee
samrous6213 <138663805+samrous6213@users.noreply.github.com>
2024-04-15 22:26:28 +0100

    Update urls.py

--- django_chatbot/chatbot/urls.py
+++ django_chatbot/chatbot/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path 
+# Importation de la fonction path depuis le module django.urls et des vues de l'application chatbot
+from django.urls import path
 from . import views
 
+# Définition des URLs de l'application Django "chatbot"
 urlpatterns = [
-            path('',views.chatbot, name='chatbot'),
-            path('login',views.login, name='login'),
-            path('register',views.register, name='register'),     
-            path('logout',views.logout, name='logout'),       
-            ]
+    # URL pour afficher la page du chatbot et gérer les interactions
+    path('', views.chatbot, name='chatbot'),
+    # URL pour afficher la page de connexion
+    path('login', views.login, name='login'),
+    # URL pour afficher la page d'inscription
+    path('register', views.register, name='register'),
+    # URL pour gérer la déconnexion de l'utilisateur
+    path('logout', views.logout, name='logout'),
+]
